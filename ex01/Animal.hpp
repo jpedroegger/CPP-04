cd32f23f125e23692435c494bd409ae2556436b5
jegger <jpedroegger@gmail.com>
2024-01-03 13:32:25 +0000

    virtual destructor

--- ex01/Animal.hpp
+++ ex01/Animal.hpp
@@ -12,7 +12,7 @@ public:
 	Animal();
 	Animal(const Animal &src);
 	Animal&	operator=(const Animal &rhs);
-	virtual	~Animal();
+	virtual ~Animal();
 
 	virtual void		makeSound(void) const;
 	std::string	getType(void) const;
--- ex01/main.cpp
+++ ex01/main.cpp
@@ -4,60 +4,47 @@
 
 int	main(void)
 {
-	/*	const Animal*		dog = new Dog();
-	std::cout << std::endl;
-	const Animal		dog2;
-	std::cout << std::endl;
-
-	//Get Brains Address
-	std::cout << "Dog:  " << dog->getBrain() << std::endl;
-	
-	std::cout << std::endl;
-	// Set Ideas
-	dog->getBrain()->setIdea("Am I a dog 1?", 1);
-	dog->getBrain()->setIdea("what a dog 1", 2);
-	// Get Ideas
-	std::cout << "Dog:  " << dog->getBrain()->getIdea(1) << std::endl;
-	std::cout << "Dog:  " << dog->getBrain()->getIdea(2) << std::endl;
-	
-	std::cout << std::endl;
-	dog2 = *dog;
-	std::cout << std::endl;
-	//Get Brains Address
-	std::cout << "Dog:  " << dog->getBrain() << std::endl;
-	
-	std::cout << std::endl;
-
-	// Get Ideas
-	std::cout << "Dog:  " << dog->getBrain()->getIdea(1) << std::endl;
-	std::cout << "Dog:  " << dog->getBrain()->getIdea(2) << std::endl;
-
-	std::cout << std::endl;
-
-	delete dog;*/
-
 	{
 	    const Animal* j = new Dog();
 		std::cout << std::endl;
 	    const Animal* i = new Cat();
 		std::cout << std::endl;
 
-	    delete j;//should not create a leak
+	    delete j;
 		std::cout << std::endl;
 	    delete i;
 		std::cout << std::endl;
+		
+		std::cout << "*********************************" << std::endl;
 
 	    Dog basic;
 	    {
 			std::cout << std::endl;
+	        
 	        Dog tmp = basic;
+			std::cout << std::endl;
+	        Dog tmp2(basic);
+			std::cout << std::endl;
+
+			std::cout << basic.getBrain() << std::endl;
+			std::cout << tmp.getBrain() << std::endl;
+			std::cout << tmp2.getBrain() << std::endl;
 			std::cout << std::endl;
 	    }
 		std::cout << std::endl;
-
+		std::cout << basic.getBrain() << std::endl;
+		basic.getBrain()->setIdea("Basic Idea", 0);
+		std::cout << basic.getBrain()->getIdea(0) << std::endl;
+	}
+	{
+		std::cout << "*********************************" << std::endl;
+		std::cout << std::endl;
 	    const Animal* animals[4] = { new Dog(), new Dog(), new Cat(), new Cat() };
+		std::cout << std::endl;
 	    for ( int i = 0; i < 4; i++ ) {
+	        animals[i]->makeSound();
 	        delete animals[i];
+			std::cout << std::endl;
 	    }
 	}
 
